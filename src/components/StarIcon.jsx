d961d86dae8cab6e35f979cc31a65df3a7261009
urosrelic <urosrelicc@gmail.com>
2024-02-28 18:39:06 +0100

    feat: add star icon

---
+++ src/components/StarIcon.jsx
@@ -0,0 +1,3 @@
+export const StarIcon = ({ icon }) => {
+  return icon;
+};
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,36 +1,40 @@
 /* eslint-disable react/prop-types */
-import { CiStar } from 'react-icons/ci';
+import { CiTrash } from 'react-icons/ci';
+import { IoIosStarOutline } from 'react-icons/io';
+import { IoIosStar } from 'react-icons/io';
+
+import { StarIcon } from './StarIcon';
 
 export const Task = ({
   taskName,
   taskId,
   completed,
+  important,
   handleSetAsImportant,
   handleComplete,
 }) => {
   return (
     <div className='task'>
       <div className='checkbox-container'>
-        <label>
-          <input
-            type='checkbox'
-            checked={completed ? true : false}
-            onChange={() => handleComplete(taskId)}
-          />
-        </label>
-      </div>
-      <div
-        className={
-          completed ? 'task-information completed' : 'task-information'
-        }
-      >
-        {taskName}
-      </div>
-      <div className='task-actions'>
-        <div className='delete-task'></div>
-        <div className='set-task-as-important'>
-          <CiStar />
+        <input
+          type='checkbox'
+          checked={completed ? true : false}
+          onChange={() => handleComplete(taskId)}
+        />
+        <div
+          className={
+            completed ? 'task-information completed' : 'task-information'
+          }
+        >
+          {taskName}
         </div>
+        <div className='star-icon'>
+          <StarIcon icon={important ? <IoIosStar /> : <IoIosStarOutline />} />
+        </div>
+      </div>
+
+      <div className='delete-task'>
+        <CiTrash id='delete-icon' />
       </div>
     </div>
   );
